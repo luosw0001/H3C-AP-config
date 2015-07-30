473d8b264c5bc21690d3ef94422c1b64530bde5b
TIW <qiaolezi.lvdoubing@gmail.com>
2015-07-30 15:20:02 +0800

    里程碑式 第一阶段完成

--- Python/H3C/count.py
+++ Python/H3C/count.py
@@ -54,5 +54,31 @@ target_list = sorted(target_list, key=lambda item: int(item[5][12:]))
 for item in target_list:
     print(item)
 
+import xlsxwriter
 
+workbook = xlsxwriter.Workbook('AP.xlsx')
+worksheet = workbook.add_worksheet()
+
+worksheet.write(1, 1, 'serial id')
+worksheet.write(1, 2, 'AP name')
+worksheet.write(1, 3, 'model')
+worksheet.write(1, 4, 'mac adddress')
+worksheet.write(1, 5, 'AP IP adddress')
+worksheet.write(1, 6, 'port')
+worksheet.write(1, 7, 'Switch')
+worksheet.write(1, 8, 'Switch IP adddress')
+
+row = 2
+col = 1
+
+for item in target_list:
+    worksheet.write(row, col, item[0])
+    worksheet.write(row, col + 1, item[1])
+    worksheet.write(row, col + 2, item[2])
+    worksheet.write(row, col + 3, item[3])
+    worksheet.write(row, col + 4, item[4])
+    worksheet.write(row, col + 5, item[5])
+    row += 1
+
+workbook.close()
 
